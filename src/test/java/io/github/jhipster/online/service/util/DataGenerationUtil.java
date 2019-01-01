87f3cfbec06ae74a2ad6bef1ed6bc7ef69909794
Julien Dubois <julien.dubois@gmail.com>
2019-01-01 18:32:05 +0100

    Update license for 2019

--- src/test/java/io/github/jhipster/online/service/util/DataGenerationUtil.java
+++ src/test/java/io/github/jhipster/online/service/util/DataGenerationUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2018 the original author or authors from the JHipster Online project.
+ * Copyright 2017-2019 the original author or authors from the JHipster Online project.
  *
  * This file is part of the JHipster Online project, see https://github.com/jhipster/jhipster-online
  * for more information.
