6031641623c93c1d78575af3cdc492219635eb04
Julien Dubois <julien.dubois@gmail.com>
2019-09-13 18:21:04 +0200

    Fix security vulnerability.
    See https://www.jhipster.tech/2019/09/13/jhipster-release-6.3.0.html

--- src/main/java/io/github/jhipster/online/service/util/RandomUtil.java
+++ src/main/java/io/github/jhipster/online/service/util/RandomUtil.java
@@ -21,6 +21,8 @@ package io.github.jhipster.online.service.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.security.SecureRandom;
+
 /**
  * Utility class for generating random Strings.
  */
@@ -28,33 +30,41 @@ public final class RandomUtil {
 
     private static final int DEF_COUNT = 20;
 
-    private RandomUtil() {
+    private static final SecureRandom SECURE_RANDOM;
+
+    static {
+        SECURE_RANDOM = new SecureRandom();
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    };
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
     }
 
     /**
      * Generate a password.
      *
-     * @return the generated password
+     * @return the generated password.
      */
     public static String generatePassword() {
-        return RandomStringUtils.randomAlphanumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
      * Generate an activation key.
      *
-     * @return the generated activation key
+     * @return the generated activation key.
      */
     public static String generateActivationKey() {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
      * Generate a reset key.
      *
-     * @return the generated reset key
+     * @return the generated reset key.
      */
     public static String generateResetKey() {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 }
