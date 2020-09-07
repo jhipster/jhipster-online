92c1504fa1d7f45a969e83508df32e467f9f3f9a
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-09-07 08:53:21 -0700

    feat: use a simplified database connection
    This changes the database connection without using the Spring GCP dependencies.
    
    Related to https://github.com/jhipster/jhipster-online/issues/230

--- src/main/java/io/github/jhipster/online/service/JHipsterService.java
+++ src/main/java/io/github/jhipster/online/service/JHipsterService.java
@@ -25,10 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
