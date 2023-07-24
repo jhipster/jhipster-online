a03cc3df8a8d924f20cf08721e130ac4ff02e51d
kubin-kloud <140262111+kubin-kloud@users.noreply.github.com>
2023-07-24 18:16:33 +0000

    Changes to work with jhipster-online backend

--- src/main/java/io/github/jhipster/online/web/rest/GeneratorResource.java
+++ src/main/java/io/github/jhipster/online/web/rest/GeneratorResource.java
@@ -113,8 +113,8 @@ public class GeneratorResource {
     public @ResponseBody ResponseEntity<byte[]> downloadApplication(@RequestBody String applicationConfiguration) {
         applicationConfiguration = SanitizeInputs.sanitizeInput(applicationConfiguration);
         log.info("Downloading application - .yo-rc.json: {}", applicationConfiguration);
-        // String applicationId = UUID.randomUUID().toString();
-        String applicationId = "1".toString();
+        String applicationId = UUID.randomUUID().toString();
+        // String applicationId = "1".toString();
         String zippedApplication;
         try {
             zippedApplication = this.generatorService.generateZippedApplication(applicationId, applicationConfiguration);
