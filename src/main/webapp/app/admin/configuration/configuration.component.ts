ee43dd844f1751b0df01a3b67116561e479a8ff2
Quentin <quentin.monmert@gmail.com>
2023-09-17 14:44:18 +0200

    Sonar: Extract this nested ternary operation into an independent statement

--- src/main/webapp/app/admin/configuration/configuration.component.ts
+++ src/main/webapp/app/admin/configuration/configuration.component.ts
@@ -43,8 +43,9 @@ export class ConfigurationComponent implements OnInit {
   }
 
   filterAndSortBeans(): void {
+    const beansAscendingSort = this.beansAscending ? -1 : 1;
     this.beans = this.allBeans
       .filter(bean => !this.beansFilter || bean.prefix.toLowerCase().includes(this.beansFilter.toLowerCase()))
-      .sort((a, b) => (a.prefix < b.prefix ? (this.beansAscending ? -1 : 1) : this.beansAscending ? 1 : -1));
+      .sort((a, b) => (a.prefix < b.prefix ? beansAscendingSort : beansAscendingSort * -1));
   }
 }
